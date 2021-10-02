fcfdf420a412e8d7a3de47721ccf7a3d95fca9ac
EllinorL <lindahlellinor@gmail.com>
2021-10-02 22:19:37 +0200

    JÄTTE UPDATE
    Massor med nytt, glömde uppdatera till Github under hela dagen :O

---
+++ script.js
@@ -0,0 +1,107 @@
+
+// MY JS 'SCROLL TO THE TOP'-BUTTON :)
+
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+
+
+
+
+
+
+
+///*––––––––––––––––––––––––––––––––––––––––––––––––––
+//YOU CAN IGNORE ANYTHING BELOW THIS LINE, IT IS NOT DONE, JUST IDEAS :)
+
+
+
+
+
+
+
+
+
+// COLLAPSIBLE ABOUT ME JS (IDEA I WANT TO IMPLEMENT)
+// Put the education/work experience tables on the HOME page but make it foldable with js
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TIMELINE JS - ABOUT ME PAGE
+(function () {
+    "use strict";
+
+    // define variables
+    var items = document.querySelectorAll(".timeline li");
+
+    // check if an element is in viewport
+    // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <=
+            (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function callbackFunc() {
+        for (var i = 0; i < items.length; i++) {
+            if (isElementInViewport(items[i])) {
+                items[i].classList.add("in-view");
+            }
+        }
+    }
+
+    // listen for events
+    window.addEventListener("load", callbackFunc);
+    window.addEventListener("resize", callbackFunc);
+    window.addEventListener("scroll", callbackFunc);
+})();
