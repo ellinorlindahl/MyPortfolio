676e3de27b3d1688d068db2ae459cc48f4c8bd80
EllinorL <lindahlellinor@gmail.com>
2021-11-03 21:30:11 +0100

    Stor remake

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 
-// MY JS 'SCROLL TO THE TOP'-BUTTON :)
 
+// Responsive navbar to hamburger
 
-//Get the button
-var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
+function myFunction() {
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
+        x.className += "responsive";
     } else {
-        mybutton.style.display = "none";
+        x.className = "navbar";
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
