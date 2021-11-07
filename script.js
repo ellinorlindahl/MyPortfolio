e47df2727761a2ff25d87b7c23e9acb1bc0acbd4
EllinorL <lindahlellinor@gmail.com>
2021-11-07 17:40:48 +0100

    ny meny etc

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 
 
-// Responsive navbar to hamburger
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 
 function myFunction() {
-    var x = document.getElementById("navbar");
-    if (x.className === "navbar") {
-        x.className += "responsive";
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
     } else {
-        x.className = "navbar";
+        x.className = "topnav";
     }
 }
-
-
-
-
-
-
-
-
 
 
 
